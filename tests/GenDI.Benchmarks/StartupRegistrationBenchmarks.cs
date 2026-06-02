b96c42825c33ea91e4bf982eaf293779048685dd
SCHIVEI ELTON <elton.schivei@edenred.com>
2026-06-02 09:30:39 -0300

    put back benchmark using

--- tests/GenDI.Benchmarks/StartupRegistrationBenchmarks.cs
+++ tests/GenDI.Benchmarks/StartupRegistrationBenchmarks.cs
@@ -1,5 +1,6 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
+using GenDI.Benchmarks.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
